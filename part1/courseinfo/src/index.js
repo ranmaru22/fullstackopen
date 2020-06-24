9653d21d5a8cb61795c80feb9703521459d5d4c8
Alex Sun <alexsun82@icloud.com>
2020-06-24 13:51:38 +0200

    Add soltions for 1.1 + 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = props => (
+    <header>
+        <h1>{props.course}</h1>
+    </header>
+);
+
+const Content = props => (
+    <div>
+        {props.parts.map(elem => <Part part={elem.name} exercises={elem.exercises} />)}
+    </div>
+);
+
+const Part = props => (
+    <p>{props.part}: {props.exercises}</p>
+);
+
+const Total = props => (
+    <p>Number of Exercises: {props.exercises.reduce((acc, val) => acc + val, 0)}</p>
+);
+
+const App = () => {
+    const course = "Half Stack application development";
+    const parts = [
+        { name: "Fundamentals of React", exercises: 10 },
+        { name: "Using props to pass data", exercises: 7 },
+        { name: "State of a component", exercises: 14 },
+    ];
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={parts} />
+            <Total exercises={[parts[0].exercises, parts[1].exercises, parts[2].exercises]} />
+        </div>
+    );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
