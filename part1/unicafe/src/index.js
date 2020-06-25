6d9646662d3a1bcd369841d96ea12ac82459f837
Alex Sun <alexsun82@icloud.com>
2020-06-25 16:13:48 +0200

    Add solution 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,8 +7,23 @@ const App = () => {
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
+    const clickHandlerGood = () => setGood(good + 1);
+    const clickHandlerNeutral = () => setNeutral(neutral + 1);
+    const clickHandlerBad = () => setBad(bad + 1);
+
     return (
-        <div></div>
+        <div>
+            <h1>Give Feedback</h1>
+            <div>
+                <button onClick={clickHandlerGood}>good</button>
+                <button onClick={clickHandlerNeutral}>neutral</button>
+                <button onClick={clickHandlerBad}>bad</button>
+            </div>
+            <h1>Statistics</h1>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+        </div>
     );
 };
 
