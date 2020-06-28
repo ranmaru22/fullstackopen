90286f11afe66a180fd0b67d6ce3afc695f8b011
Alex Sun <alexsun82@icloud.com>
2020-06-28 10:21:25 +0200

    Add solution 2.6

--- part2/phonebook/src/app.js
+++ part2/phonebook/src/app.js
@@ -4,18 +4,32 @@ const App = () => {
     const [ persons, setPersons ] = useState([{ name: "Arto Hellas" }]);
     const [ newName, setNewName ] = useState("");
 
+    const addToPhonebook = e => {
+        e.preventDefault();
+        const newPerson = { name: newName };
+        setPersons(persons.concat(newPerson));
+        setNewName("");
+    };
+
+    const newNameHandler = e => setNewName(e.target.value);
+
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
+            <form onSubmit={addToPhonebook}>
                 <div>
-                    name: <input />
+                    name: <input value={newName} onChange={newNameHandler} />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
+                <ul>
+                    {persons.map(person => <li key={person.name}>{person.name}</li>)}
+                </ul>
+            <h2>Debug</h2>
+            <div>{newName}</div>
         </div>
     );
 };
