6a54deca93099ead2cc38f5431f1f8407349884d
Alex Sun <alexsun82@icloud.com>
2020-07-01 23:06:01 +0200

    Add UI build

---
+++ part3/phonebook-backend/build/precache-manifest.dc44123bcb4d79c0a107c17ae868e6e4 3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654481d5b4a2027d49199ecc87011d77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4da43e3a57e93fa3832",
+    "url": "/static/css/main.283c9707.chunk.css"
+  },
+  {
+    "revision": "5430bfbaf0e8a39436b3",
+    "url": "/static/js/2.a5e1a041.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5e1a041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4da43e3a57e93fa3832",
+    "url": "/static/js/main.6af39074.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
