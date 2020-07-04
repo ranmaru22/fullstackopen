2b345080329d41e1788c1efb8e7abc9898924fa7
Alex Sun <alexsun82@icloud.com>
2020-07-04 18:37:47 +0200

    Add solution 4.5

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -2,4 +2,11 @@ const dummy = blogs => 1;
 
 const totalLikes = blogs => blogs.reduce((acc, val) => acc + val.likes, 0);
 
-export default { dummy, totalLikes };
+const favorite = blogs =>
+    blogs.length === 0
+        ? {}
+        : blogs.reduce((max, val) => (val.likes > max.likes ? val : max), {
+              likes: 0
+          });
+
+export default { dummy, totalLikes, favorite };
