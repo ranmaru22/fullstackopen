b27cfe444841653982e23f9f03fbb34826c1ee5f
Alex Sun <alexsun82@icloud.com>
2020-07-09 17:55:23 +0200

    Add solution 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import LoginForm from "./components/LoginForm";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([]);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then(blogs => setBlogs(blogs));
+    }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+    if (!user) {
+        return (
+            <div>
+                <h2>blogs</h2>
+                <p>Please log in</p>
+                <div>
+                    <LoginForm setUserFn={setUser} />
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h2>blogs</h2>
+                <p>Logged in as {user.name ?? user.username}</p>
+                <div>
+                    {blogs.map(blog => (
+                        <Blog key={blog.id} blog={blog} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+};
 
-export default App
+export default App;
+
---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import loginService from "../services/login";
+
+const LoginForm = ({ setUserFn }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async e => {
+        e.preventDefault();
+        const user = await loginService.login({ username, password });
+        if (user) {
+            setUserFn(user);
+        }
+    };
+
+    return (
+        <form onSubmit={handleLogin}>
+            <div>
+                Username:{" "}
+                <input
+                    id="username"
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                Password:{" "}
+                <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+    );
+};
+
+export default LoginForm;
