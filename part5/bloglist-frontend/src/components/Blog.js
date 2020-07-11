7b73c1d2e421aa12a4160afee744bd8da63a7249
Alex Sun <alexsun82@icloud.com>
2020-07-11 16:50:05 +0200

    Add solution 5.6 + 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+import "./Blog.css";
 
-export default Blog
+const Blog = ({ blog }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const toggleVisible = () => setIsVisible(!isVisible);
+
+    return (
+        <div className="blog">
+            <div>
+                <strong>{blog.title}</strong> <em>{blog.author}</em>
+                <button onClick={toggleVisible}>
+                    {isVisible ? "hide details" : "show details"}
+                </button>
+            </div>
+            <div className={isVisible ? "" : "hidden"}>
+                <p>
+                    <a href={blog.url}>{blog.url}</a>
+                </p>
+                <p>
+                    {blog.likes} likes <button>Like</button>
+                </p>
+                <p>Added by: {blog.user.name ?? blog.user.username}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
