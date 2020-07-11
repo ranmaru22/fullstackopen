fb8bcfe1b4bc2b4a0755c287d0e22c35c3ae452f
Alex Sun <alexsun82@icloud.com>
2020-07-11 15:13:14 +0200

    Add solution 5.5

--- part5/bloglist-frontend/src/components/NewBlogForm.js
+++ part5/bloglist-frontend/src/components/NewBlogForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import blogService from "../services/blogs";
+import "./NewBlogForm.css";
 
 const NewBlogForm = ({ user, blogs, setBlogsFn, cb }) => {
+    const [isVisible, setIsVisible] = useState(false);
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
     const [url, setUrl] = useState("");
+
+    const toggleVisible = () => setIsVisible(!isVisible);
 
     const handleSubmit = async e => {
         e.preventDefault();
@@ -15,6 +19,7 @@ const NewBlogForm = ({ user, blogs, setBlogsFn, cb }) => {
             setTitle("");
             setAuthor("");
             setUrl("");
+            toggleVisible();
             cb(`Added blog ${savedBlog.title} by ${savedBlog.author}`);
         } catch {
             cb("Error adding blog.", true);
@@ -22,39 +27,49 @@ const NewBlogForm = ({ user, blogs, setBlogsFn, cb }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                Title:
-                <input
-                    id="title"
-                    type="text"
-                    name="title"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                />
+        <div>
+            <div className={isVisible ? "hidden" : ""}>
+                <button onClick={toggleVisible}>Add new Blog</button>
             </div>
-            <div>
-                Author:
-                <input
-                    id="author"
-                    type="text"
-                    name="author"
-                    value={author}
-                    onChange={e => setAuthor(e.target.value)}
-                />
+            <div className={isVisible ? "" : "hidden"}>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        Title:
+                        <input
+                            id="title"
+                            type="text"
+                            name="title"
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        Author:
+                        <input
+                            id="author"
+                            type="text"
+                            name="author"
+                            value={author}
+                            onChange={e => setAuthor(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        URL:
+                        <input
+                            id="url"
+                            type="text"
+                            name="url"
+                            value={url}
+                            onChange={e => setUrl(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Create</button>
+                    <button type="reset" onClick={toggleVisible}>
+                        Cancel
+                    </button>
+                </form>
             </div>
-            <div>
-                URL:
-                <input
-                    id="url"
-                    type="text"
-                    name="url"
-                    value={url}
-                    onChange={e => setUrl(e.target.value)}
-                />
-            </div>
-            <button type="submit">Create</button>
-        </form>
+        </div>
     );
 };
 
