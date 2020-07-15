bfc7d209c011497aea36d0db69bd410c33fd92bf
Alex Sun <alexsun82@icloud.com>
2020-07-15 11:18:53 +0200

    Add solution 5.19

--- part5/bloglist-frontend/cypress/integration/app.spec.js
+++ part5/bloglist-frontend/cypress/integration/app.spec.js
@@ -31,4 +31,25 @@ describe("Blog app", () => {
             cy.contains("Error logging in").should("exist").should("have.class", "error-msg");
         });
     });
+
+    describe.only("When logged in", () => {
+        beforeEach(() => {
+            const testUser = { username: "root", password: "Sekret123" };
+            cy.request("POST", "http://localhost:3001/api/login", testUser).then(res => {
+                localStorage.setItem("blogAppUser", JSON.stringify(res.body));
+                cy.visit("http://localhost:3000");
+            });
+        });
+
+        it("can create new blogs", () => {
+            cy.contains("Add new Blog").click();
+            const testBlog = { title: "test blog title", author: "Foo Bar", url: "http://foo.bar" };
+            cy.get("#addBlogForm").as("blogForm").should("exist");
+            cy.get("@blogForm").get("input#title").type(testBlog.title);
+            cy.get("@blogForm").get("input#author").type(testBlog.author);
+            cy.get("@blogForm").get("input#url").type(testBlog.url);
+            cy.get("@blogForm").get("button[type='submit']").click();
+            cy.contains(`${testBlog.title} ${testBlog.author}`).should("exist");
+        });
+    });
 });
