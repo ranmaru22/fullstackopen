9dd8eb761e2a891a5f067838a0e9d004b80c7802
Alex Sun <alexsun82@icloud.com>
2020-07-20 16:15:42 +0200

    Add solution 7.4

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Route, Switch, Link, useHistory, useRouteMatch } from "react-router-dom";
+import { useField } from "./hooks";
 
 const Menu = () => {
     const padding = {
@@ -77,21 +78,21 @@ const Footer = () => (
 );
 
 const CreateNew = props => {
-    const [content, setContent] = useState("");
-    const [author, setAuthor] = useState("");
-    const [info, setInfo] = useState("");
+    const content = useField("text");
+    const author = useField("text");
+    const info = useField("text");
     const history = useHistory();
 
     const handleSubmit = e => {
         e.preventDefault();
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         });
         history.push("/");
-        props.cb(`Created new anecdote: ${content}`);
+        props.cb(`Created new anecdote: ${content.value}`);
     };
 
     return (
@@ -100,19 +101,15 @@ const CreateNew = props => {
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input
-                        name="content"
-                        value={content}
-                        onChange={e => setContent(e.target.value)}
-                    />
+                    <input {...content} />
                 </div>
                 <div>
                     author
-                    <input name="author" value={author} onChange={e => setAuthor(e.target.value)} />
+                    <input {...author} />
                 </div>
                 <div>
                     url for more info
-                    <input name="info" value={info} onChange={e => setInfo(e.target.value)} />
+                    <input {...info} />
                 </div>
                 <button>create</button>
             </form>
---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export const useField = type => {
+    const [value, setValue] = useState("");
+    const onChange = e => setValue(e.target.value);
+    return { type, value, onChange };
+};
