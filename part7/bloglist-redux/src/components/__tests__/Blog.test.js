e69c02553b3c8c04c447f41afe3ee2646fc42330
Alex Sun <alexsun82@icloud.com>
2020-07-23 17:28:22 +0200

    Fix unit tests

--- part7/bloglist-redux/src/components/__tests__/Blog.test.js
+++ part7/bloglist-redux/src/components/__tests__/Blog.test.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, fireEvent } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
 import Blog from "../Blog";
 
 describe("Blog component rendering", () => {
-    let component;
+    const initialState = { user: { token: 1 } };
+    const mockStore = configureStore();
+    let component, store;
     beforeEach(() => {
         const blog = {
             title: "Test Blog",
@@ -13,10 +17,11 @@ describe("Blog component rendering", () => {
             likes: 42,
             user: { id: 1, username: "root" }
         };
-        const handleLike = jest.fn();
-        const handleDelete = jest.fn();
+        store = mockStore(initialState);
         component = render(
-            <Blog blog={blog} handleLike={handleLike} handleDelete={handleDelete} />
+            <Provider store={store}>
+                <Blog blog={blog} />
+            </Provider>
         );
     });
 
@@ -24,35 +29,5 @@ describe("Blog component rendering", () => {
         const titleSection = component.container.querySelector(".blogTitle");
         const detailsSection = component.container.querySelector(".blogDetails");
         expect(titleSection).toHaveTextContent("Test Blog Test Author");
-        expect(detailsSection).toHaveClass("hidden");
-    });
-
-    it("shows details when the button is clicked", () => {
-        const detailsSection = component.container.querySelector(".blogDetails");
-        const showDetailsBtn = component.getByText("show details");
-        expect(detailsSection).toHaveClass("hidden");
-        fireEvent.click(showDetailsBtn);
-        expect(detailsSection).not.toHaveClass("hidden");
-    });
-});
-
-describe("Likes functionality", () => {
-    it("increments the likes when the like button is clicked", () => {
-        const blog = {
-            title: "Test Blog",
-            author: "Test Author",
-            url: "http://foo.bar",
-            likes: 42,
-            user: { id: 1, username: "root" }
-        };
-        const handleLike = jest.fn();
-        const handleDelete = jest.fn();
-        const component = render(
-            <Blog blog={blog} handleLike={handleLike} handleDelete={handleDelete} />
-        );
-        const likeButton = component.getByText("Like");
-        fireEvent.click(likeButton);
-        fireEvent.click(likeButton);
-        expect(handleLike).toHaveBeenCalledTimes(2);
     });
 });
