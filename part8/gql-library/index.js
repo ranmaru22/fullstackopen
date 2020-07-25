6ce8f2c8b9e0e48907c93cb88b7a6990b14ab803
Alex Sun <alexsun82@icloud.com>
2020-07-25 11:43:23 +0200

    Add solution 8.2

--- part8/gql-library/index.js
+++ part8/gql-library/index.js
@@ -80,16 +80,26 @@ let books = [
 ];
 
 const typeDefs = gql(`
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-  }
+    type Book {
+        title: String!
+        published: Int!
+        author: String!
+        genres: [String]!
+        id: ID!
+    }
+    
+    type Query {
+        bookCount: Int!
+        authorCount: Int!
+        allBooks: [Book!]!
+    }
 `);
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
+        authorCount: () => authors.length,
+        allBooks: () => books
     }
 };
 
