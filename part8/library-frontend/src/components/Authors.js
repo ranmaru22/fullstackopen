c435591e7ef6dc7210c2b26113c1661928169a22
Alex Sun <alexsun82@icloud.com>
2020-07-26 17:15:01 +0200

    Refactor code

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,59 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { useQuery, useMutation } from "@apollo/client";
-import { ALL_AUTHORS, EDIT_BIRTHYEAR } from "../queries";
-
-const EditForm = () => {
-    const { data, loading, error } = useQuery(ALL_AUTHORS);
-    const [authorList, setAuthorList] = useState([]);
-    const [name, setName] = useState("");
-    const [born, setBorn] = useState("");
-
-    useEffect(() => {
-        if (!loading && !error) {
-            setAuthorList(data.allAuthors.map(a => a.name));
-        }
-    }, [data, error, loading]);
-
-    const [editBirthyear] = useMutation(EDIT_BIRTHYEAR, {
-        refetchQueries: [{ query: ALL_AUTHORS }]
-    });
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        editBirthyear({ variables: { name, setBornTo: Number(born) } });
-        setName("");
-        setBorn("");
-    };
-
-    return (
-        <div>
-            <h3>Edit Author's Birth Year</h3>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <select name="name" value={name} onChange={e => setName(e.target.value)}>
-                        {authorList.map(author => (
-                            <option key={author} value={author}>
-                                {author}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="born">Born: </label>
-                    <input
-                        type="text"
-                        name="born"
-                        value={born}
-                        onChange={e => setBorn(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <button type="submit">Update author</button>
-                </div>
-            </form>
-        </div>
-    );
-};
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS } from "../queries";
+import EditForm from "./EditForm";
 
 const Authors = ({ show }) => {
     const results = useQuery(ALL_AUTHORS);
