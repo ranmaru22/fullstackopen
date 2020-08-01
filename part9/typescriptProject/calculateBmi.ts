01e79723775da16eaceb5dbfdad194e84b7e06d6
Alex Sun <alexsun82@icloud.com>
2020-08-01 11:32:10 +0200

    Add solution 9.1

---
+++ part9/typescriptProject/calculateBmi.ts
@@ -0,0 +1,19 @@
+const calculateBmi = (height: number, weight: number): string => {
+    const heightInM = height / 100;
+    const bmi = weight / (heightInM * heightInM);
+    if (bmi <= 15) {
+        return "Very severely underweight";
+    } else if (bmi <= 16) {
+        return "Severely underweight";
+    } else if (bmi <= 18.5) {
+        return "Underweight";
+    } else if (bmi <= 25) {
+        return "Normal (healthy weight)";
+    } else if (bmi <= 30) {
+        return "Overweight";
+    } else {
+        return "Obese";
+    }
+};
+
+console.log(calculateBmi(180, 74));
