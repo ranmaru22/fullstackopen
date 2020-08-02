a3c14abe4ba08d905fed8788ed48d3b05f22196c
Alex Sun <alexsun82@icloud.com>
2020-08-02 10:16:43 +0200

    Add solution 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (_req: Request, res: Response) => {
+app.get("/api/ping", (_req: Request, res: Response) => {
     res.send("pong");
 });
 
