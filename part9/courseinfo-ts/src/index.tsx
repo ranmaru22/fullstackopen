1c99879ed4b3cbbc858983bcfdd1a55638acedae
Alex Sun <alexsun82@icloud.com>
2020-08-03 10:00:40 +0200

    Setup + remove unnecessary files

--- part9/courseinfo-ts/src/index.tsx
+++ part9/courseinfo-ts/src/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App: React.FC = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <h1>{courseName}</h1>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
