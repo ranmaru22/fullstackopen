12795310ff58a8adc1f22426ed18c4103f7a3a33
Alex Sun <alexsun82@icloud.com>
2020-08-03 10:20:20 +0200

    Add solution 9.14

--- part9/courseinfo-ts/src/index.tsx
+++ part9/courseinfo-ts/src/index.tsx
@@ -1,41 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App: React.FC = () => {
-  const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
+const Header: React.FC<{ name: string }> = ({ name }) => {
+    return <h1>{name}</h1>;
+};
 
-  return (
-    <div>
-      <h1>{courseName}</h1>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-      <p>
-        Number of exercises{" "}
-        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
-      </p>
-    </div>
-  );
+interface Course {
+    name: string;
+    exerciseCount: number;
+}
+
+const Content: React.FC<{ courseList: Course[] }> = ({ courseList }) => {
+    return (
+        <div>
+            {courseList.map(c => (
+                <p key={c.name}>
+                    {c.name} {c.exerciseCount}
+                </p>
+            ))}
+        </div>
+    );
+};
+
+const Total: React.FC<{ total: number }> = ({ total }) => {
+    return <p>Number of exercises: {total}</p>;
+};
+
+const App: React.FC = () => {
+    const courseName = "Half Stack application development";
+    const courseParts = [
+        {
+            name: "Fundamentals",
+            exerciseCount: 10
+        },
+        {
+            name: "Using props to pass data",
+            exerciseCount: 7
+        },
+        {
+            name: "Deeper type usage",
+            exerciseCount: 14
+        }
+    ];
+    const total = courseParts.reduce((carry, part) => carry + part.exerciseCount, 0);
+
+    return (
+        <>
+            <Header name={courseName} />
+            <Content courseList={courseParts} />
+            <Total total={total} />
+        </>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
