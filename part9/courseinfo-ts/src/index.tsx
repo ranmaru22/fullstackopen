80b6fac1c9807ff159d9c22898170a68baa4469d
Alex Sun <alexsun82@icloud.com>
2020-08-03 15:52:06 +0200

    Add solution 9.15

--- part9/courseinfo-ts/src/index.tsx
+++ part9/courseinfo-ts/src/index.tsx
@@ -5,41 +5,127 @@ const Header: React.FC<{ name: string }> = ({ name }) => {
     return <h1>{name}</h1>;
 };
 
-interface Course {
+interface CoursePartBase {
     name: string;
     exerciseCount: number;
 }
 
-const Content: React.FC<{ courseList: Course[] }> = ({ courseList }) => {
+interface CoursePartWithDesc extends CoursePartBase {
+    description: string;
+}
+
+interface CoursePartOne extends CoursePartWithDesc {
+    name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+    name: "Using props to pass data";
+    groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartWithDesc {
+    name: "Deeper type usage";
+    exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends CoursePartWithDesc {
+    name: "The Awesomeness of Typescript";
+    recommendedReading: string[];
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+const Part: React.FC<{ part: CoursePart }> = ({ part }) => {
+    switch (part.name) {
+        case "Fundamentals": {
+            return (
+                <div>
+                    <h2>{part.name}</h2>
+                    <p>{part.description}</p>
+                    <p>Exercises: {part.exerciseCount}</p>
+                </div>
+            );
+        }
+        case "Using props to pass data":
+            return (
+                <div>
+                    <h2>{part.name}</h2>
+                    <p>Exercises: {part.exerciseCount}</p>
+                    <p>Group Projects: {part.groupProjectCount}</p>
+                </div>
+            );
+        case "Deeper type usage":
+            return (
+                <div>
+                    <h2>{part.name}</h2>
+                    <p>{part.description}</p>
+                    <p>Exercises: {part.exerciseCount}</p>
+                    <p>Link: {part.exerciseSubmissionLink}</p>
+                </div>
+            );
+        case "The Awesomeness of Typescript":
+            return (
+                <div>
+                    <h2>{part.name}</h2>
+                    <p>{part.description}</p>
+                    <p>Exercises: {part.exerciseCount}</p>
+                    Recommended Reading:
+                    <ul>
+                        {part.recommendedReading.map((r, i) => (
+                            <li key={i}>{r}</li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        default:
+            ((x: never): never => {
+                throw new Error(`Unhandled union member: ${JSON.stringify(x)}`);
+            })(part);
+    }
+};
+
+const Content: React.FC<{ courseList: CoursePart[] }> = ({ courseList }) => {
     return (
         <div>
             {courseList.map(c => (
-                <p key={c.name}>
-                    {c.name} {c.exerciseCount}
-                </p>
+                <Part key={c.name} part={c} />
             ))}
         </div>
     );
 };
 
 const Total: React.FC<{ total: number }> = ({ total }) => {
-    return <p>Number of exercises: {total}</p>;
+    return <p>Total number of exercises: {total}</p>;
 };
 
 const App: React.FC = () => {
     const courseName = "Half Stack application development";
-    const courseParts = [
+    const courseParts: CoursePart[] = [
         {
             name: "Fundamentals",
-            exerciseCount: 10
+            exerciseCount: 10,
+            description: "This is an awesome course part"
         },
         {
             name: "Using props to pass data",
-            exerciseCount: 7
+            exerciseCount: 7,
+            groupProjectCount: 3
         },
         {
             name: "Deeper type usage",
-            exerciseCount: 14
+            exerciseCount: 14,
+            description: "Confusing description",
+            exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+        },
+        {
+            name: "The Awesomeness of Typescript",
+            exerciseCount: 12,
+            description: "Typescript is indeed awesome",
+            recommendedReading: [
+                "Typescript basics",
+                "More Typescript basics",
+                "Advanced Typescript"
+            ]
         }
     ];
     const total = courseParts.reduce((carry, part) => carry + part.exerciseCount, 0);
